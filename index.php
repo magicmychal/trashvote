8dab407cc16b2935cbaf3d8e07a1a545ba47bc39
Michal PAwlicki <michalpawlicki@outlook.com>
2017-05-17 14:23:46 +0200

    added analytycs trakcr plus fixed region list, deleted unnecessary index html

--- index.php
+++ index.php
@@ -404,5 +404,14 @@
 	<script src="js/jquery.onepagenav.js"></script>
 	<script src="js/main.js"></script>
 </body>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
+  ga('create', 'UA-89483375-3', 'auto');
+  ga('send', 'pageview');
+
+</script>
 </html>
